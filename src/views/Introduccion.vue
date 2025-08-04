9b499ef043001517062db64d9e74b9b0f88e3f1b
acearaujo <hola@alvaroaraujo.com>
2025-08-04 10:40:24 -0500

    Actualziar video, Fuentes y DU

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -19,7 +19,7 @@
 
    
     .video
-      iframe(width="560" height="315" src="https://www.youtube.com/embed/32KQw66yGbY?si=9ufc5FlxjfV092T8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      iframe(width="560" height="315" src="https://www.youtube.com/embed/G9IuZQ8xQT4?si=wA6b5T4YQvPNESGE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     figcaption Nota: Sena (2024).  
 
 </template>
