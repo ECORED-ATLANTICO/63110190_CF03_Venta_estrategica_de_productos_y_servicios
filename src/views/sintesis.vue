95baae6871e9a0e0da75f060309e7bfd8050d422
acearaujo <hola@alvaroaraujo.com>
2025-08-26 17:20:49 -0500

    ajuste de sintesis y nuevo DU

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -6,14 +6,14 @@
     .row.justify-content-center.align-items-center.mb-2(data-aos="zoom-in")
       .col-lg-2.col-md-5.mb-4.col-6
         figure
-          img(src='@/assets/curso/img_sintesis.svg', alt='El componente formativo Fidelización y postventa de productos y servicios se enfoca en fortalecer competencias clave para la atención y permanencia del cliente, mediante el uso de herramientas, estrategias y técnicas orientadas a la fidelización, evaluación de la satisfacción y gestión efectiva de quejas, reclamos y sugerencias, promoviendo así una mejora continua en la experiencia del cliente y relaciones comerciales duraderas desde un enfoque técnico y práctico.')
+          img(src='@/assets/curso/img_sintesis.svg', alt='Imagen decorativa')
       .col-lg-10.mb-4
         p El componente formativo Fidelización y postventa de productos y servicios se enfoca en fortalecer competencias clave para la atención y permanencia del cliente, mediante el uso de herramientas, estrategias y técnicas orientadas a la fidelización, evaluación de la satisfacción y gestión efectiva de quejas, reclamos y sugerencias, promoviendo así una mejora continua en la experiencia del cliente y relaciones comerciales duraderas desde un enfoque técnico y práctico.
 
     .row.justify-content-center
       .col-lg-12.mb-5.bgs.p-4.brad
         figure
-          img(src="@/assets/curso/sintesis.svg", alt="Imagen decorativa")
+          img(src="@/assets/curso/sintesis.svg", alt="El componente formativo Fidelización y postventa de productos y servicios se enfoca en fortalecer competencias clave para la atención y permanencia del cliente, mediante el uso de herramientas, estrategias y técnicas orientadas a la fidelización, evaluación de la satisfacción y gestión efectiva de quejas, reclamos y sugerencias, promoviendo así una mejora continua en la experiencia del cliente y relaciones comerciales duraderas desde un enfoque técnico y práctico.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
